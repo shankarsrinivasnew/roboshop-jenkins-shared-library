235dceff5c33e5dcd875ecc517712054c3abc33f
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-06 00:15:24 +0530

    sdfg

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,23 +2,23 @@ def call() {
     pipeline {
         agent any
         parameters {
-      string(name: 'ENV', defaultValue: 'prod', description: 'Which Environment?')
-      string(name: 'ACTION', defaultValue: 'init', description: 'Which Action?')
-    }
+            string(name: 'ENV', defaultValue: 'prod', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: 'init', description: 'Which Action?')
+        }
         stages {
             stage('init') {
                 steps {
-                    terraform ${ACTION} -backend-config /env-${ENV}/state.tfvars
+                    sh 'terraform ${ACTION} -backend-config /env-${ENV}/state.tfvars'
                 }
             }
             stage("plan") {
                 steps {
-                    terraform ${ACTION} -var-file=/env-${ENV}/${ENV}.tfvars
+                    sh 'terraform ${ACTION} -var-file=/env-${ENV}/${ENV}.tfvars'
                 }
             }
             stage("apply") {
                 steps {
-                    terraform ${ACTION} -auto-approve -var-file=/env-${ENV}/${ENV}.tfvars
+                    sh 'terraform ${ACTION} -auto-approve -var-file=/env-${ENV}/${ENV}.tfvars'
                 }
             }
         }
