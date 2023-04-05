db811995a1bf90ecb204e8de4a5d08e97792d747
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-06 00:45:42 +0530

    dfg

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -7,17 +7,26 @@ def call() {
         }
         stages {
             stage('init') {
+                when {
+                    environment name: 'ACTION', value: 'init'
+                }
                 steps {
                     sh 'terraform ${ACTION} -backend-config=env-${ENV}/state.tfvars'
                 }
             }
             stage("plan") {
+                when {
+                    environment name: 'ACTION', value: 'plan'
+                }
                 steps {
                     sh 'terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars'
                 }
             }
             stage("apply") {
                 steps {
+                    when {
+                        environment name: 'ACTION', value: 'apply'
+                    }
                     // sh 'terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars'
                     echo "apply done"
                 }
