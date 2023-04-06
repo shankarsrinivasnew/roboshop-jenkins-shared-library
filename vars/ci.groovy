2abf7b391503a45f7206d4c63dddc0c800f2367d
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-06 22:47:56 +0530

    sdf

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,9 +2,13 @@ def call() {
     pipeline {
         agent any
         stages {
-            stage('compile') {
+            stage('compile/build') {
+                when {
+                        environment name: "app_lang", value: "nodejs"
+                    }
                 steps {
                     echo "compile"
+                    sh 'npm install'
                 }
             }
             stage('test') {
