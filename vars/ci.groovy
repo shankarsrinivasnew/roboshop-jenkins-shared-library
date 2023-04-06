729addfa90c8dec8cb398c107e2110c9713c97d7
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-07 00:12:10 +0530

    dsfg

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,33 +5,18 @@ def call() {
             stage('compile/build') {
                 steps {
                     script {
-                        if (app_lang == "nodejs") {
-                            sh 'npm install'
-                        }
-                        if (app_lang == "maven") {
-                            sh 'mvn package'
-                        }
-                        if (app_lang == "python") {
-                            sh 'sudo pip3.6 install -r requirements.txt'
-                        }
-                        // if (app_lang == "go") {
-                        //     sh '''
-                        //     go mod init dispatch
-                        //     go get
-                        //     go build
-                        //     '''
-                        // }
+                        common.compile()
                     }
                 }
             }
             stage('test') {
-                steps {
-                    echo "test"
+                steps script {
+                    common.test()
                 }
             }
             stage('codequality') {
-                steps {
-                    echo "codequality"
+                steps script {
+                    common.codequality()
                 }
             }
         }
---
+++ vars/common.groovy
@@ -0,0 +1,28 @@
+def compile() {
+    if (app_lang == "nodejs") {
+        sh 'npm install'
+    }
+    if (app_lang == "maven") {
+        sh 'mvn package'
+    }
+    if (app_lang == "python") {
+        sh 'sudo pip3.6 install -r requirements.txt'
+    }
+    // if (app_lang == "go") {
+    //     sh '''
+    //     go mod init dispatch
+    //     go get
+    //     go build
+    //     '''
+    // }
+}
+
+
+def test() {
+    echo "Testing ok"
+}
+
+
+def codequality() {
+    echo "codequality ok"
+}
