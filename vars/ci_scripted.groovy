b609902a811f0bb679bf93a98f85d9fac3638aef
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-11 13:06:33 +0530

    sadf

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -17,7 +17,7 @@ def call() {
                     common.compile()
                 }
             }
-            if (!env.TAG_NAME ) {
+            if (!env.TAG_NAME && env.BRANCH_NAME != "main") {
                 stage('testcases') {
                     common.testcases()
                 }
