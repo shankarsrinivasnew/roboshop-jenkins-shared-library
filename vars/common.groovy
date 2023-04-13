9a631f78ce718fd2d3930e428173a5e4fd741169
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-13 16:45:33 +0530

    dsfg

--- vars/common.groovy
+++ vars/common.groovy
@@ -46,12 +46,12 @@ def codequality() {
 
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VERSION'
-    // if (app_lang == "maven") {
-    //     sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION"
-    // }
-    // else {
-    //     sh "zip -r ${component}-${TAG_NAME}.zip $ * -x Jenkinsfile"
-    // }
+    if (app_lang == "maven") {
+        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+    }
+    else {
+        sh 'zip -r ${component}-${TAG_NAME}.zip $ * -x Jenkinsfile'
+    }
 }
 
 def uploadArtifacts() {
