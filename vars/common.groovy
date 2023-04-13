950d1404c1a53577252820dc226174ee1ef720c7
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-13 16:48:49 +0530

    sdfg

--- vars/common.groovy
+++ vars/common.groovy
@@ -49,9 +49,9 @@ def prepareArtifacts() {
     if (app_lang == "maven") {
         sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION"
     }
-    // else {
-    //     sh 'zip -r ${component}-${TAG_NAME}.zip $ * -x Jenkinsfile'
-    // }
+    else {
+        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
+    }
 }
 
 def uploadArtifacts() {
