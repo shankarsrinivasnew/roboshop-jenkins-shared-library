7aa5e94c1808d77ab79d4e6cd07c098a2fc9f35e
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-14 13:31:51 +0530

    sdfgh

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -18,7 +18,7 @@ def call() {
             stage('deploy servers') {
                 steps {
                     sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}_${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
-                    sh 'ansible-playbook -i /tmp/servers -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=DevOps321'
+                    sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=DevOps321'
                 }
             }
         }
