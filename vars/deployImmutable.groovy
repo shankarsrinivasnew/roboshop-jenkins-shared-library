6c4c43952c9ef086c09b0356148c54fe5793d4b8
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-19 12:38:31 +0530

    sdf

---
+++ vars/deployImmutable.groovy
@@ -0,0 +1,33 @@
+def call() {
+    pipeline {
+        agent any
+        options {
+            ansiColor('xterm')
+        }
+        parameters {
+            string(name: 'environment', defaultValue: 'prod', description: 'Environment')
+            string(name: 'component', defaultValue: '', description: 'component name ')
+            string(name: 'app_version', defaultValue: '', description: 'Application tag version')
+        }
+        stages {
+            stage('update parameter store') {
+                steps {
+                    sh 'aws ssm put-parameter --name "${environment}.${component}_app_version" --type "String" --value "${app_version}" --overwrite'
+                }
+            }
+            stage('deploy servers') {
+                steps {
+                    script {
+                        sh "aws autoscaling start-instance-refresh --auto-scaling-group-name ${component}-${environment}-asg --preferences '{"InstanceWarmup": 180, "MinHealthyPercentage": 90}'"
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
+}
